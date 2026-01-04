4c5485171bd48174dff032233a57d7f4b93b7d63
sehkone <sehkone@cluml.com>
2026-01-04 13:59:17 +0900

    Implement Rust agent core logic
    - Implement CLI parsing with Clap
    - Implement eAB credentials loading
    - Implement CSR generation with rcgen
    - Refactor to remove all clippy allowances

--- agent/target/debug/deps/bootroot_agent-c71cfb37f41bbead.d
+++ agent/target/debug/deps/bootroot_agent-c71cfb37f41bbead.d
@@ -1,5 +1,7 @@
-/Users/sehkone/projects/bootroot/agent/target/debug/deps/bootroot_agent-c71cfb37f41bbead.d: src/main.rs
+/Users/sehkone/projects/bootroot/agent/target/debug/deps/bootroot_agent-c71cfb37f41bbead.d: src/main.rs src/acme.rs src/eab.rs
 
-/Users/sehkone/projects/bootroot/agent/target/debug/deps/libbootroot_agent-c71cfb37f41bbead.rmeta: src/main.rs
+/Users/sehkone/projects/bootroot/agent/target/debug/deps/libbootroot_agent-c71cfb37f41bbead.rmeta: src/main.rs src/acme.rs src/eab.rs
 
 src/main.rs:
+src/acme.rs:
+src/eab.rs:
